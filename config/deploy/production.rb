111ce45aee8a592018f8df7d3fa4848f57dc2555
DannhiNT <nhi.nguyentrandan@gmail.com>
2025-07-04 14:09:32 +0700

    Bind puma to a unix socket

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,5 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+server "46.137.198.182", user: "deploy", roles: %w[app db web], ssh_options: { forward_agent: true }
--- config/puma.rb
+++ config/puma.rb
@@ -39,3 +39,6 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+# Bind Puma to a UNIX socket
+bind "unix:///home/deploy/apps/mysocialsite/shared/tmp/sockets/puma.sock"
