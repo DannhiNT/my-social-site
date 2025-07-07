c42ce2ec904fa6d93b0216e2c30a47817ae16fea
DannhiNT <nhi.nguyentrandan@gmail.com>
2025-07-07 15:46:48 +0700

    Added skip_before_action :verify_authenticity_token inside the application controller to resolve CSRF token error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :configure_permitted_parameters, if: :devise_controller?
